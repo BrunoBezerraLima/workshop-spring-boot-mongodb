efcb0a4493a2b84480750364831214f57bde3a94
Bruno Bezerra Lima <brunobezerralima@gmail.com>
2020-06-17 17:26:51 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/brunobezerralima/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.brunobezerralima.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brunobezerralima.workshopmongo.domain.User;
+import com.brunobezerralima.workshopmongo.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
