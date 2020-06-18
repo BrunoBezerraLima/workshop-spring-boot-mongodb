ccb842b9c7c9e9e328c0cfef80e0060217d104e6
Bruno Bezerra Lima <brunobezerralima@gmail.com>
2020-06-17 21:02:44 -0300

    Obtendo um usuario por id

--- src/main/java/com/brunobezerralima/workshopmongo/services/UserService.java
+++ src/main/java/com/brunobezerralima/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.brunobezerralima.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brunobezerralima.workshopmongo.domain.User;
 import com.brunobezerralima.workshopmongo.repositories.UserRepository;
+import com.brunobezerralima.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
